a435df5a59958a7e34721e84be9a6d8ad04e2a68
SerhiiKurdesha <bellini1988@gmail.com>
2021-10-22 23:09:04 +0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-    $('.slider').slick();
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        speed: 500,
+        easing: 'ease',
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnDotsHover: true,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    autoplay: false,
+                }
+            }
+        ]
+    });
 });
+
