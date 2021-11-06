83a6a417e33356b31ecaed258480135f4ac5ceb0
SerhiiKurdesha <bellini1988@gmail.com>
2021-11-06 14:27:09 +0200

    add scss

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+const name = "Сергей Курдеша";
+let age = 33;
+let boo = true;
+
+function simpleMath(a, b) {
+    let result = a + b;
+    return result;
+}
+
+let sum = simpleMath(334, 759);
+
+console.log(sum);
